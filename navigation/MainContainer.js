4a6fe5765b7c30a6dd9079abae7de511903892b1
Jibin Gallistus Gnanadhas <jibingallistus@gmail.com>
2024-08-17 00:15:31 +1000

    initial commit

---
+++ navigation/MainContainer.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View } from "react-native";
+
+export default function MainContainer() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
