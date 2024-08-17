ddcf733d599caf219fc47a2ad9cd94d3a57d8aee
Gurmehar Singh <gsingh@waheguroo.com>
2024-08-18 01:12:03 +1000

    components

---
+++ components/Item.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Card, Title, Paragraph } from "react-native-paper";
+
+export default function Item() {
+    return (
+        <Card>
+            <Card.Content>
+                <Title>Card title</Title>
+                <Paragraph>Card content</Paragraph>
+            </Card.Content>
+        </Card>
+    );
+}
