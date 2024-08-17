39195ef8dc64322a4f65881e5c52357eb46e04c0
Gurmehar Singh <gsingh@waheguroo.com>
2024-08-18 01:18:26 +1000

    added react-native-paper

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,9 +1,13 @@
+import Item from '@/components/Item';
+import Search from '@/components/Searchbar';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function Tab() {
   return (
     <View style={styles.container}>
+      <Search />
       <Text>Home</Text>
+      <Item />
     </View>
   );
 }
